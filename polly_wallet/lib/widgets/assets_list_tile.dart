ff935e7bdea2b535865ff625763049a2dab100c7
Akshit Ostwal <akshitostwal@gmail.com>
2021-01-03 15:35:22 +0530

    added slidable action in asset tile

--- polly_wallet/lib/widgets/assets_list_tile.dart
+++ polly_wallet/lib/widgets/assets_list_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class AssetsListTile extends StatelessWidget {
   final String iconURL;
@@ -10,30 +11,51 @@ class AssetsListTile extends StatelessWidget {
       {this.name, this.shortName, this.iconURL, this.amount, this.price});
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.network(
-        iconURL,
-      ),
-      trailing: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            '\$$amount',
-            style: TextStyle(
-              color: Colors.black,
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.25,
+      key: Key('abc'),
+      child: ListTile(
+        leading: Image.network(
+          iconURL,
+        ),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '\$$amount',
+              style: TextStyle(
+                color: Colors.black,
+              ),
             ),
-          ),
-          Text(
-            price,
-            style: TextStyle(color: Colors.grey, fontSize: 12),
-          ),
-        ],
+            Text(
+              price,
+              style: TextStyle(color: Colors.grey, fontSize: 12),
+            ),
+          ],
+        ),
+        title: Text(name),
+        subtitle: Text(
+          shortName,
+          style: TextStyle(color: Colors.grey, fontSize: 12),
+        ),
       ),
-      title: Text(name),
-      subtitle: Text(
-        shortName,
-        style: TextStyle(color: Colors.grey, fontSize: 12),
-      ),
+      actions: <Widget>[
+        IconSlideAction(
+          caption: 'Deposit',
+          color: Colors.green,
+          icon: Icons.archive,
+          onTap: () => print('Deposit'),
+        ),
+      ],
+      secondaryActions: <Widget>[
+        IconSlideAction(
+          caption: 'send',
+          color: Colors.blue,
+          icon: Icons.send,
+          onTap: () => print('send'),
+        ),
+      ],
     );
   }
 }
